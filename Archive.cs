4518e4c176f4469570087d12630673555c87363a
crskycode <crskycode@hotmail.com>
2021-04-05 13:43:44 +0800

    Add project files.

---
+++ Archive.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGI_Pack_Tool
+{
+    static class Archive
+    {
+        public static void Create(string filePath, string folderPath)
+        {
+            using (Stream stream = File.Create(filePath))
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                var version = Encoding.ASCII.GetBytes("BURIKO ARC20");
+                writer.Write(version);
+
+                var entries = new List<Entry>();
+
+                // Grab entries
+                foreach (var path in Directory.EnumerateFiles(folderPath))
+                {
+                    var info = new FileInfo(path);
+
+                    if (info.Length == 0)
+                        continue;
+                    if (info.Name.Length >= 0x60)
+                        continue;
+
+                    var entry = new Entry();
+                    entry.Path = path;
+                    entry.Name = info.Name;
+                    entry.Size = Convert.ToUInt32(info.Length);
+
+                    entries.Add(entry);
+                }
+
+                writer.Write(entries.Count);
+
+                var encoding = Encoding.GetEncoding("utf-8");
+
+                uint offset = 0;
+
+                // Write entry index
+                foreach (var entry in entries)
+                {
+                    // Write name
+                    var name = encoding.GetBytes(entry.Name);
+                    writer.Write(name);
+
+                    // Write padding zero bytes
+                    int padding = 0x60 - name.Length;
+                    if (padding > 0)
+                        writer.Write(new byte[padding]);
+
+                    // Write entry information
+                    writer.Write(offset);
+                    writer.Write(entry.Size);
+                    writer.Write(0);
+                    writer.Write(0);
+                    writer.Write(0);
+                    writer.Write(0);
+                    writer.Write(0);
+                    writer.Write(0);
+
+                    offset += entry.Size;
+                }
+
+                // Write entry data
+                foreach (var entry in entries)
+                {
+                    var data = File.ReadAllBytes(entry.Path);
+                    writer.Write(data);
+                }
+
+                writer.Flush();
+            }
+        }
+
+        struct Entry
+        {
+            public string Path;
+            public string Name;
+            public uint Size;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGI_Pack_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("  Create archive : BGI_Pack_Tool <folder> ...");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach (var path in args)
+            {
+                if (!Util.PathIsFolder(path))
+                    continue;
+
+                try
+                {
+                    var filePath = Path.ChangeExtension(path, ".arc");
+                    var fileName = Path.GetFileName(filePath);
+
+                    Console.WriteLine($"Creating archive {fileName}");
+
+                    Archive.Create(filePath, path);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
